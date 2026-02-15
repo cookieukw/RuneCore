bbad691703095ce1ef7f11b05757001dd9893e83
cookieukw <cookieukw@users.noreply.github.com>
2026-02-15 17:55:23 -0300

    feat: Add numerous new essence item definitions and register them in CoreEssences.

--- src/main/java/com/cookie/runecore/content/CoreEssences.java
+++ src/main/java/com/cookie/runecore/content/CoreEssences.java
@@ -7,10 +7,25 @@ public class CoreEssences {
     public static void init() {
         RuneCore core = RuneCore.get();
 
-        // Register Fire Essence
         core.registerEssence(new Essence("essence_fire", RuneElement.FIRE, 1));
-        
-        // Register Poison Essence
+        core.registerEssence(new Essence("essence_earth", RuneElement.EARTH, 1));
+        core.registerEssence(new Essence("essence_wind", RuneElement.WIND, 1));
+        core.registerEssence(new Essence("essence_water", RuneElement.WATER, 1));
+        core.registerEssence(new Essence("essence_ice", RuneElement.ICE, 1));
+        core.registerEssence(new Essence("essence_lightning", RuneElement.LIGHTNING, 1));
+        core.registerEssence(new Essence("essence_light", RuneElement.LIGHT, 1));
+        core.registerEssence(new Essence("essence_shadow", RuneElement.SHADOW, 1));
+        core.registerEssence(new Essence("essence_life", RuneElement.LIFE, 1));
+        core.registerEssence(new Essence("essence_death", RuneElement.DEATH, 1));
+        core.registerEssence(new Essence("essence_mind", RuneElement.MIND, 1));
+        core.registerEssence(new Essence("essence_blood", RuneElement.BLOOD, 1));
+        core.registerEssence(new Essence("essence_chaos", RuneElement.CHAOS, 1));
+        core.registerEssence(new Essence("essence_aether", RuneElement.AETHER, 1));
+        core.registerEssence(new Essence("essence_void", RuneElement.VOID, 1));
+        core.registerEssence(new Essence("essence_time", RuneElement.TIME, 1));
+        core.registerEssence(new Essence("essence_metal", RuneElement.METAL, 1));
+        core.registerEssence(new Essence("essence_crystal", RuneElement.CRYSTAL, 1));
         core.registerEssence(new Essence("essence_poison", RuneElement.POISON, 1));
+        core.registerEssence(new Essence("essence_acid", RuneElement.ACID, 1));
     }
 }
