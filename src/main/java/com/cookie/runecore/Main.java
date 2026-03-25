2924017e096404a39061fbd8922a26870a75da51
cookieukw <cookieukw@users.noreply.github.com>
2026-03-25 19:40:48 -0300

    refactor: rename RuneCore.java to Main.java

--- src/main/java/com/cookie/runecore/Main.java
+++ src/main/java/com/cookie/runecore/Main.java
@@ -4,24 +4,30 @@ import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import com.cookie.runecore.commands.RuneStatsCommand;
 import com.cookie.runecore.commands.TestUICommand;
+import com.cookie.runecore.systems.CastListener;
 import com.cookie.runecore.systems.MobDropSystem;
 import com.cookie.runecore.systems.ui.ManaHudManager;
 import com.cookie.runecore.commands.CustomTimeCommand;
+import com.cookie.runecore.system.RuneCore;
 
 import javax.annotation.Nonnull;
 
-public class RuneCore extends JavaPlugin {
+public class Main extends JavaPlugin {
 
-    public RuneCore(@Nonnull JavaPluginInit init) {
+    public Main(@Nonnull JavaPluginInit init) {
         super(init);
     }
 
     @Override
     protected void setup() {
+        // Initialize RuneCore engine defaults
+        RuneCore.get().initDefaults();
+
         this.getCommandRegistry().registerCommand(new RuneStatsCommand());
         this.getCommandRegistry().registerCommand(new CustomTimeCommand());
         this.getCommandRegistry().registerCommand(new TestUICommand());
         this.getEntityStoreRegistry().registerSystem(new MobDropSystem());
         new ManaHudManager(this.getEventRegistry());
+        new CastListener(this.getEventRegistry());
     }
 }
