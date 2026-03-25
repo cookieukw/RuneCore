d23487336463eb78c252ee252bc9092ce0a92812
cookieukw <cookieukw@users.noreply.github.com>
2026-03-25 01:07:57 -0300

    refactor: update core essences

--- src/main/java/com/cookie/runecore/content/CoreEssences.java
+++ src/main/java/com/cookie/runecore/content/CoreEssences.java
@@ -7,25 +7,25 @@ public class CoreEssences {
     public static void init() {
         RuneCore core = RuneCore.get();
 
-        core.registerEssence(new Essence("essence_fire", RuneElement.FIRE, 1));
-        core.registerEssence(new Essence("essence_earth", RuneElement.EARTH, 1));
-        core.registerEssence(new Essence("essence_wind", RuneElement.WIND, 1));
-        core.registerEssence(new Essence("essence_water", RuneElement.WATER, 1));
-        core.registerEssence(new Essence("essence_ice", RuneElement.ICE, 1));
-        core.registerEssence(new Essence("essence_lightning", RuneElement.LIGHTNING, 1));
-        core.registerEssence(new Essence("essence_light", RuneElement.LIGHT, 1));
-        core.registerEssence(new Essence("essence_shadow", RuneElement.SHADOW, 1));
-        core.registerEssence(new Essence("essence_life", RuneElement.LIFE, 1));
-        core.registerEssence(new Essence("essence_death", RuneElement.DEATH, 1));
-        core.registerEssence(new Essence("essence_mind", RuneElement.MIND, 1));
-        core.registerEssence(new Essence("essence_blood", RuneElement.BLOOD, 1));
-        core.registerEssence(new Essence("essence_chaos", RuneElement.CHAOS, 1));
-        core.registerEssence(new Essence("essence_aether", RuneElement.AETHER, 1));
-        core.registerEssence(new Essence("essence_void", RuneElement.VOID, 1));
-        core.registerEssence(new Essence("essence_time", RuneElement.TIME, 1));
-        core.registerEssence(new Essence("essence_metal", RuneElement.METAL, 1));
-        core.registerEssence(new Essence("essence_crystal", RuneElement.CRYSTAL, 1));
-        core.registerEssence(new Essence("essence_poison", RuneElement.POISON, 1));
-        core.registerEssence(new Essence("essence_acid", RuneElement.ACID, 1));
+        core.registerEssence(new Essence("Essence_Fire", RuneElement.FIRE, 1));
+        core.registerEssence(new Essence("Essence_Earth", RuneElement.EARTH, 1));
+        core.registerEssence(new Essence("Essence_Wind", RuneElement.WIND, 1));
+        core.registerEssence(new Essence("Essence_Water", RuneElement.WATER, 1));
+        core.registerEssence(new Essence("Essence_Ice", RuneElement.ICE, 1));
+        core.registerEssence(new Essence("Essence_Lightning", RuneElement.LIGHTNING, 1));
+        core.registerEssence(new Essence("Essence_Light", RuneElement.LIGHT, 1));
+        core.registerEssence(new Essence("Essence_Shadow", RuneElement.SHADOW, 1));
+        core.registerEssence(new Essence("Essence_Life", RuneElement.LIFE, 1));
+        core.registerEssence(new Essence("Essence_Death", RuneElement.DEATH, 1));
+        core.registerEssence(new Essence("Essence_Mind", RuneElement.MIND, 1));
+        core.registerEssence(new Essence("Essence_Blood", RuneElement.BLOOD, 1));
+        core.registerEssence(new Essence("Essence_Chaos", RuneElement.CHAOS, 1));
+        core.registerEssence(new Essence("Essence_Aether", RuneElement.AETHER, 1));
+        core.registerEssence(new Essence("Essence_Void", RuneElement.VOID, 1));
+        core.registerEssence(new Essence("Essence_Time", RuneElement.TIME, 1));
+        core.registerEssence(new Essence("Essence_Metal", RuneElement.METAL, 1));
+        core.registerEssence(new Essence("Essence_Crystal", RuneElement.CRYSTAL, 1));
+        core.registerEssence(new Essence("Essence_Poison", RuneElement.POISON, 1));
+        core.registerEssence(new Essence("Essence_Acid", RuneElement.ACID, 1));
     }
 }
